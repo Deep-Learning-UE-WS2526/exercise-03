b3e47b2a1eea1572bfeb14c75135b493757492ca
Gabriel Pospischil <gabriel-pospischil@t-online.de>
2025-11-03 18:53:53 +0100

    finished exercise

--- functions.py
+++ functions.py
@@ -9,8 +9,37 @@ def func1(a, b):
     return None
   return type(a)
 
-func1(1,2)
-func1("Welt", "Hallo")
-func1(None, None)
-func1(11, "Freunde")
-func1(5.4, 3.6)
+# func1(1,2)
+# func1("Welt", "Hallo")
+# func1(None, None)
+# func1(11, "Freunde")
+# func1(5.4, 3.6)
+
+def func2(*args):
+   numArgs = len(args)
+   if numArgs < 2:
+     return str(numArgs)
+   if numArgs == 2:
+     return func1(args[0], args[1])
+   if numArgs > 2:
+     return numArgs
+
+print("func2:")
+print(func2(8))
+print(func2(5,5))
+print(func2(2,5,3))
+
+def func3(**kwargs):
+  if all(key in kwargs for key in("a", "b")):
+    return func1(kwargs.get("a"), kwargs.get("b"))
+  else: 
+    args = []
+    for value in kwargs.values():
+      args.append(value)
+    return func2(*args)
+
+print("func3")
+print(func3(a= 2, b=5))
+print(func3(a = "world", b = "hello"))
+print(func3(a = 5, b = 10, c = 15))
+print(func3(d = 2, x = 10))
--- listcomprehension.py
+++ listcomprehension.py
@@ -1,3 +1,14 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [i*2 for i in x] # all elements of `x`, multiplied by 2
+print(a)
+b = [i for i in x if i%2==0] # all even elements of `x`
+print(b)
+c = [x for x in y if x==True] # all truish elements of `y` (i.e., all elements that evaluate to `True` in a boolean context)
+print(c)
+d = [x for x in y if isinstance(x, str)] # all string elements of `y`
+print(d)
+e = [[True for i in range(ee)] for ee in x if ee%2!=0] # contains a list for each odd element of `x`
+print(e)
+
