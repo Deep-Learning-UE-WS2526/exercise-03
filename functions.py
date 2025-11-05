03c0a66eb0a9557a09bf88bce7d4c70d312d230b
parwely <paul.speier@web.de>
2025-11-05 19:32:40 +0100

    initial commit

--- functions.py
+++ functions.py
@@ -8,9 +8,50 @@ def func1(a, b):
   if type(a) != type(b):
     return None
   return type(a)
+ 
 
-func1(1,2)
-func1("Welt", "Hallo")
-func1(None, None)
-func1(11, "Freunde")
-func1(5.4, 3.6)
+def func2(*args):
+  """Handle a variable number of positional arguments.
+
+  - If less than two arguments: return a string with the number of arguments.
+  - If exactly two arguments: pass them to func1 and return the result.
+  - If more than two arguments: return the number of arguments as an int.
+  """
+  n = len(args)
+  if n < 2:
+    return str(n)
+  if n == 2:
+    return func1(args[0], args[1])
+  return n
+
+
+def func3(**kwargs):
+  """Handle a variable number of named arguments.
+
+  - If kwargs contains both 'a' and 'b', pass them to func1.
+  - Otherwise, pass all argument names (strings) to func2.
+  """
+  if 'a' in kwargs and 'b' in kwargs:
+    return func1(kwargs['a'], kwargs['b'])
+  # pass all names of the arguments into func2
+  names = list(kwargs.keys())
+  return func2(*names)
+
+
+if __name__ == '__main__':
+  # demonstrations (showing the different return modes)
+  print(func1(1,2))                # ints -> sum
+  print(func1("Welt", "Hallo")) # strings -> concat (b + ' ' + a)
+  print(func1(None, None))         # both None -> message
+  print(func1(11, "Freunde"))    # different types -> None
+  print(func1(5.4, 3.6))           # unmatched same type -> return type
+
+  # func2
+  print(func2())                   # less than two -> string '0'
+  print(func2(10))                 # less than two -> string '1'
+  print(func2(2,3))                # two args -> passed to func1 -> 5
+  print(func2(1,2,3))              # more than two -> int 3
+
+  # func3
+  print(func3(a=5, b=7))           # has a and b -> func1(5,7) -> 12
+  print(func3(x=1, y=2))           # no a/b -> pass names to func2 -> func2('x','y') -> func1('x','y') -> 'y x'
--- listcomprehension.py
+++ listcomprehension.py
@@ -1,3 +1,26 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+# A list called `a` that contains all elements of `x`, multiplied by 2.
+a = [item * 2 for item in x]
+
+# A list called `b` that contains all even elements of `x`.
+b = [item for item in x if item % 2 == 0]
+
+# A list called `c` that contains all truish elements of `y`.
+c = [item for item in y if bool(item)]
+
+# A list called `d` that contains all string elements of `y`.
+d = [item for item in y if isinstance(item, str)]
+
+# A list called `e` that contains a list for each odd element of `x`.
+# Each inner list contains `True` repeated `n` times where `n` is the odd value.
+e = [[True] * item for item in x if item % 2 == 1]
+
+if __name__ == '__main__':
+	print("a:", a)
+	print("b:", b)
+	print("c:", c)
+	print("d:", d)
+	print("e:", e)
+
