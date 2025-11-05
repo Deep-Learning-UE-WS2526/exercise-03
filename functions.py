c238b1e13a46ded512376caa0097324a970166f4
faran230 <130466482+faran230@users.noreply.github.com>
2025-11-05 22:35:17 +0100

    Exercise 03

--- functions.py
+++ functions.py
@@ -1,16 +1,26 @@
 def func1(a, b):
-  if type(a) == int and type(b) == int:
-    return a+b
-  if type(a) == str and type(b) == str:
-    return b+" "+a
-  if a is None and b is None:
-    return "Does not exist."
-  if type(a) != type(b):
-    return None
-  return type(a)
+    if type(a) == int and type(b) == int:
+        return a + b
+    if type(a) == str and type(b) == str:
+        return b + " " + a
+    if a is None and b is None:
+        return "Does not exist."
+    if type(a) != type(b):
+        return None
+    return type(a)
 
-func1(1,2)
-func1("Welt", "Hallo")
-func1(None, None)
-func1(11, "Freunde")
-func1(5.4, 3.6)
+
+def func2(*args):
+    n = len(args)
+    if n < 2:
+        return str(n)           
+    if n == 2:
+        return func1(args[0], args[1])
+    return n                   
+
+def func3(**kwargs):
+    if 'a' in kwargs and 'b' in kwargs:
+        return func1(kwargs['a'], kwargs['b'])
+   
+    return func2(*kwargs.keys())
+
